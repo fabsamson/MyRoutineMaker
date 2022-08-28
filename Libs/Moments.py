b5ac81515a2e1e4b7cb602878c57fa7a561ddc4a
samson <fabian.samson@jp.amundi.com>
2022-08-29 00:55:56 +0900

    Ajout des enums moments

---
+++ Libs/Moments.py
@@ -0,0 +1,14 @@
+from enum import enum
+class Moments(enum):
+    Lever = 1
+    DebutMatin = 2
+    Matin = 3
+    FinMatin = 4
+    Midi = 5
+    DebutAprem = 6
+    Aprem = 7
+    FinAprem = 8
+    Repas = 9
+    DebutSoiree = 10
+    Soiree = 11
+    Coucher = 12
